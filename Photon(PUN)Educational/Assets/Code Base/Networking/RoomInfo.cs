e1f0c0bfa6593b6d0546f4f234aba66b0de2e842
Serhii Karpenko <SerhiiKarpenko@users.noreply.github.com>
2023-03-20 22:51:39 +0100

    Updated scripts, added new script RoomInfo. Updated Launcher scene UI. Added new prefab Room Info
    Launcher scene now have exit button.
    
    New script RoomInfo have 3 methods from the LocalPlayerNetworkController OnPlayerEnteredRoom, OnPlayerLeftRoom, LoadArena. So now we have devided one script to two diffrent scripts with different features.
    
    New prefab Room Info just a container for RoomInfo script, each of the scenes for player's (Room for 1 etc) has it's own Room Info prefab

---
+++ Photon(PUN)Educational/Assets/Code Base/Networking/RoomInfo.cs
@@ -0,0 +1,44 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+namespace Code_Base.Networking
+{
+	public class RoomInfo : MonoBehaviourPunCallbacks
+	{
+		public override void OnPlayerEnteredRoom(Photon.Realtime.Player newPlayer)
+		{
+			Debug.LogFormat("OnPlayerEnteredRoom() {0}", newPlayer.NickName);
+
+			if (PhotonNetwork.IsMasterClient)
+			{
+				Debug.LogFormat("OnPlayerEnteredRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient);
+				
+				LoadArena();
+			}
+		}
+
+		public override void OnPlayerLeftRoom(Photon.Realtime.Player otherPlayer)
+		{
+			Debug.LogFormat("OnPlayerLeftRoom {0}", otherPlayer.NickName);
+			
+			if (PhotonNetwork.IsMasterClient)
+			{
+				Debug.LogFormat("OnPlayerLetfRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient);
+				
+				LoadArena();
+			}
+		}
+		
+		private void LoadArena()
+		{
+			if (!PhotonNetwork.IsMasterClient)
+			{
+				Debug.LogError("PhotonNetwork: Trying to lad a level but we are not the master Client(HOST)");
+				return;
+			}
+			
+			Debug.LogFormat("PhotonNetwork: loading level {0}", PhotonNetwork.CurrentRoom.PlayerCount);
+			PhotonNetwork.LoadLevel("Room for " + PhotonNetwork.CurrentRoom.PlayerCount);
+		}
+	}
+}
--- Photon(PUN)Educational/Assets/Code Base/UI/LauncherUIController.cs
+++ Photon(PUN)Educational/Assets/Code Base/UI/LauncherUIController.cs
@@ -1,5 +1,4 @@
 using Code_Base.Networking;
-using System;
 using UnityEngine;
 using UnityEngine.UI;
 
